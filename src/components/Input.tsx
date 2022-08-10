6abb8b9f779d4b1ee0d66d2d40cbfdaf4075c916
Alexander Volkov <AV-html@yandex.ru>
2022-08-10 21:34:58 +0300

    add(Input): universal component Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,56 @@
+import React, {ChangeEvent, DetailedHTMLProps, FC, InputHTMLAttributes, KeyboardEvent} from 'react';
+import s from './Input.module.css'
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type PropsType = DefaultInputPropsType & {
+    error?: string
+    errorClassName?: string
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+}
+
+const Input: FC<PropsType> = (
+    {
+        type,
+        onChange,
+        onChangeText,
+        onKeyPress,
+        onEnter,
+        className,
+        error,
+        errorClassName,
+        ...restProps
+    }
+) => {
+    const finalClassName = `${s.default} ${className}`
+    const finalErrorClassName = `${s.errorDefault} ${errorClassName}`
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+        onChangeText && onChangeText(e.currentTarget.value)
+    };
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        onKeyPress && onKeyPress(e);
+        onEnter && e.key === 'Enter' && onEnter()
+    };
+
+    return (
+        <>
+            <input
+                type={'text'}
+                onChange={onChangeHandler}
+                onKeyDown={onKeyDownHandler}
+                className={finalClassName}
+                {...restProps}
+            />
+            {
+                error &&
+                <div className={finalErrorClassName}>
+                    {error}
+                </div>
+            }
+        </>
+    );
+};
+
+export default Input;
