823056c254f12600cc8940c0c4133658afa32aab
Alexander Volkov <AV-html@yandex.ru>
2022-08-12 23:49:04 +0300

    add(InputRange): universal component InputRange

---
+++ src/components/InputRange.tsx
@@ -0,0 +1,38 @@
+import React, {ChangeEvent, DetailedHTMLProps, FC, InputHTMLAttributes} from 'react';
+import s from './InputRange.module.css'
+
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type PropsType = DefaultInputPropsType & {
+    onChangeRange?: (value: number) => void
+}
+
+const InputRange: FC<PropsType> = (
+    {
+        type,
+        onChange, onChangeRange,
+        className,
+        ...restProps
+    }
+) => {
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+        onChangeRange && onChangeRange(+e.currentTarget.value)
+    }
+
+    const finalClassName = `${s.range} ${className}`
+
+    return (
+        <>
+            <input
+                type={'range'}
+                onChange={onChangeCallback}
+                className={finalClassName}
+
+                {...restProps}
+            />
+        </>
+    )
+}
+
+export default InputRange
