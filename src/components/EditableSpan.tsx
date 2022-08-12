8fbb851e0c1a16e1e2f06ac050595cbbcf04d04d
Alexander Volkov <AV-html@yandex.ru>
2022-08-12 23:48:16 +0300

    add(EditableSpan): universal component EditableSpan

---
+++ src/components/EditableSpan.tsx
@@ -0,0 +1,81 @@
+import React, {
+    DetailedHTMLProps,
+    FC,
+    HTMLAttributes,
+    InputHTMLAttributes,
+    FocusEvent,
+    MouseEvent,
+    useState
+} from 'react'
+import s from './EditableSpan.module.css'
+import Input from './Input';
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type DefaultSpanPropsType = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+
+
+type PropsType = DefaultInputPropsType & {
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: string
+
+    spanClassName?: string
+    spanProps?: DefaultSpanPropsType
+}
+
+const EditableSpan: FC<PropsType> = (
+    {
+        autoFocus,
+        onBlur,
+        onEnter,
+        spanProps,
+
+        ...restProps
+    }
+) => {
+    const [editMode, setEditMode] = useState(false)
+    const {children, onDoubleClick, className, ...restSpanProps} = spanProps || {}
+
+    const onBlurHandler = (e: FocusEvent<HTMLInputElement>) => {
+        setEditMode(false)
+        onBlur && onBlur(e)
+    }
+
+    const onEnterHandler = () => {
+        setEditMode(false)
+        onEnter && onEnter()
+    }
+
+    const onDoubleClickHandler = (e: MouseEvent<HTMLSpanElement>) => {
+        setEditMode(true)
+        onDoubleClick && onDoubleClick(e)
+    }
+
+    const spanClassName = `${s.default} ${className}`
+
+    return (
+        <>
+            {
+                editMode ? (
+                    <Input
+                        autoFocus
+                        onBlur={onBlurHandler}
+                        onEnter={onEnterHandler}
+
+                        {...restProps}
+                    />
+                ) : (
+                    <span
+                        onDoubleClick={onDoubleClickHandler}
+                        className={spanClassName}
+                        {...restSpanProps}
+                    >
+                        {children || restProps.value}
+                    </span>
+                )
+            }
+        </>
+    )
+}
+
+export default EditableSpan
