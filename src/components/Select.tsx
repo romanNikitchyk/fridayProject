b85edfeb4768f5ada05f5f062899de47a656e794
Alexander Volkov <AV-html@yandex.ru>
2022-08-12 23:48:33 +0300

    add(Select): universal component Select

---
+++ src/components/Select.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent, DetailedHTMLProps, FC, SelectHTMLAttributes} from 'react';
+import s from './Select.module.css'
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+type PropsType = DefaultSelectPropsType & {
+    options?: string[]
+    onChangeOption?: (option: string) => void
+}
+
+const Select: FC<PropsType> = (
+    {
+        options,
+        onChange, onChangeOption,
+        className,
+        ...restProps
+    }
+) => {
+    const mappedOptions = options ? options.map((op, i) => {
+        return <option key={i} value={op}
+        >{op}</option>
+    }) : []
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChangeOption && onChangeOption(e.currentTarget.value)
+        onChange && onChange(e)
+    }
+
+    const finalClassName = `${s.default} ${className}`
+
+    return (
+        <span className={finalClassName}>
+            <select
+                onChange={onChangeCallback}
+                {...restProps}
+            >
+                {mappedOptions}
+            </select>
+        </span>
+    )
+}
+
+
+export default Select
