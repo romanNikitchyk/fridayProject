e7f77921a66bda95ca2a676bbc2d5ee33cc9b870
ZherdevMak <Zherdev.mak@gmail.com>
2022-08-21 12:09:16 +0300

    feat(Login) verification added

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -2,12 +2,30 @@ import React from 'react'
 import stl from './Login.module.css'
 import { useFormik } from 'formik'
 import Input from '../../../common/components/Input/Input'
+type FormikErrorType = {
+  email?: string
+  password?: string
+}
 
 export function Login() {
   const formik = useFormik({
     initialValues: {
       email: '',
       password: '',
+    },
+    validate: (values) => {
+      const errors: FormikErrorType = {}
+      if (!values.email) {
+        errors.email = 'Required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+      }
+      if (!values.password) {
+        errors.password = 'Required'
+      } else if (values.password.length < 3) {
+        errors.password = 'Password should be at least 3 simbols'
+      }
+      return errors
     },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
@@ -28,6 +46,9 @@ export function Login() {
             onChange={formik.handleChange}
             value={formik.values.email}
           />
+          {formik.touched.email && formik.errors.email ? (
+            <div style={{ color: 'red' }}>{formik.errors.email}</div>
+          ) : null}
           <label htmlFor="password"></label>
           <Input
             id="password"
@@ -36,7 +57,9 @@ export function Login() {
             onChange={formik.handleChange}
             value={formik.values.password}
           />
-
+          {formik.touched.password && formik.errors.password && (
+            <div style={{ color: 'red' }}>{formik.errors.password}</div>
+          )}
           <button type="submit">Submit</button>
         </form>
       </div>
