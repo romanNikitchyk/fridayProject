712184418806ac451c4a66a2739cbb84a194a62b
ZherdevMak <Zherdev.mak@gmail.com>
2022-08-22 19:24:02 +0300

    App(Store) LoginActionType added

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,11 +3,12 @@ import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { rootReducer } from './rootReducer'
 import { FirstActionsType } from '../features/auth/authReducer'
 import { SecondActionsType } from '../features/profile/profileReducer'
+import { LoginActionsType } from '../features/auth/Login/loginReducer'
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
 // ***** types ***** //
-export type AppActionsType = FirstActionsType | SecondActionsType
+export type AppActionsType = FirstActionsType | SecondActionsType | LoginActionsType
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionsType>
 export type AppThunk<ReturnType = void> = ThunkAction<
