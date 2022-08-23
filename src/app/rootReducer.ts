73369e7b2ef840aa8e4e51359da1b29875b7fe3b
Alexander Volkov <AV-html@yandex.ru>
2022-08-23 16:01:35 +0300

    add(registerReducer): created registerReducer + TC

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,8 +1,10 @@
 import { profileReducer } from '../features/profile/profileReducer'
 import { combineReducers } from 'redux'
+import { registerReducer } from '../features/auth/Register/registerReducer'
 import { loginReducer } from '../features/auth/Login/loginReducer'
 
 export const rootReducer = combineReducers({
+  register: registerReducer,
   login: loginReducer,
   profile: profileReducer,
 })
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,19 @@
 import { applyMiddleware, legacy_createStore } from 'redux'
 import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { rootReducer } from './rootReducer'
-import { FirstActionsType } from '../features/auth/authReducer'
+import { RegisterActionsType } from '../features/auth/Register/registerReducer'
 import { ProfileActionsType } from '../features/profile/profileReducer'
 import { LoginActionsType } from '../features/auth/Login/loginReducer'
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
 
 // ***** types ***** //
-export type AppActionsType = FirstActionsType | ProfileActionsType | LoginActionsType
+export type AppActionsType = RegisterActionsType | ProfileActionsType | LoginActionsType
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ThunkDispatch<RootState, unknown, AppActionsType>
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  AppActionsType
->
+    ReturnType,
+    RootState,
+    unknown,
+    AppActionsType
+    >
