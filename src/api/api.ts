a1ca7195ef4baf49e7adaa80974c4699b172a414
ZherdevMak <Zherdev.mak@gmail.com>
2022-08-26 11:44:02 +0300

    feat(ForgetPassword) error fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,11 +3,11 @@ import { ResponseType } from '../features/auth/Login/loginApi'
 
 export const instance = axios.create({
   // baseURL: 'http://localhost:7542/2.0/',
-  // baseURL: 'https://neko-back.herokuapp.com/2.0',
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:7542/2.0'
-      : 'https://neko-back.herokuapp.com/2.0',
+  baseURL: 'https://neko-back.herokuapp.com/2.0',
+  // baseURL:
+  //   process.env.NODE_ENV === 'development'
+  //     ? 'http://localhost:7542/2.0'
+  //     : 'https://neko-back.herokuapp.com/2.0',
   withCredentials: true,
 })
 
