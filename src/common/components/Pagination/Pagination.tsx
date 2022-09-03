637c2be7e4678bccbff5e9a49c4c25a08e6f5516
Alexander Volkov <AV-html@yandex.ru>
2022-09-03 14:47:13 +0300

    update(packs): pagination component

--- src/common/components/Pagination/Pagination.tsx
+++ src/common/components/Pagination/Pagination.tsx
@@ -21,22 +21,27 @@ export const Pagination: FC<PropsType> = ({
   optionsPageSize,
 }) => {
   let isEnd = false
+  let isStart = true
+  let firstPageIdx = 1
+  let lastPageIdx = 5
+
   const pages: number[] = []
-
   const pageCount = Math.ceil(totalCount / pageSize)
-
   for (let i = 0; i <= pageCount; i++) {
     pages.push(i)
   }
 
-  let firstPageIdx = currentPage - 3 < 1 ? 1 : currentPage - 2
-  const lastPageIdx = currentPage - 3 < 1 ? 5 : currentPage + 2
-
+  if (currentPage - 3 >= 1) {
+    firstPageIdx = currentPage - 2
+    isStart = false
+  }
+  if (currentPage - 3 >= 1) {
+    lastPageIdx = currentPage + 2
+  }
   if (currentPage + 3 > pageCount) {
     firstPageIdx = pageCount - 4
     isEnd = true
   }
-
   const slicedPages = pages.slice(firstPageIdx, lastPageIdx + 1)
 
   const onPreviousChangedHandler = () => {
@@ -48,44 +53,74 @@ export const Pagination: FC<PropsType> = ({
   const onLastChangedHandler = () => {
     onPageChanged(pageCount)
   }
+  const onFirstChangedHandler = () => {
+    onPageChanged(1)
+  }
 
   const selectOptions = optionsPageSize ? optionsPageSize : [`${pageSize}`, '10', '15', '20']
 
   return (
     <div className={styles.pagination}>
       <button
+        className={`${styles.previousBtn} ${styles.arrow}`}
         onClick={onPreviousChangedHandler}
-        className={styles.previousBtn}
         disabled={currentPage === 1}
+        type={'button'}
       />
-      {slicedPages.map((page, i) => {
-        const onPageChangedHandler = () => {
-          onPageChanged(page)
-        }
-        const isSelectedPage = page === currentPage
-        return (
-          <button
-            key={i}
-            className={`${styles.defaultPage} ${isSelectedPage ? styles.selectedPage : ''}`}
-            type={'button'}
-            disabled={isSelectedPage}
-            onClick={onPageChangedHandler}
-          >
-            {page}
-          </button>
-        )
-      })}
-      <button disabled className={styles.points} hidden={isEnd}>
-        ...
+
+      <button
+        className={`${styles.defaultBtnPage} ${styles.firstPage}`}
+        onClick={onFirstChangedHandler}
+        hidden={isStart}
+        type={'button'}
+      >
+        1
       </button>
-      <button onClick={onLastChangedHandler} className={styles.lastPage} hidden={isEnd}>
+
+      <span className={styles.points} hidden={isStart}>
+        ...
+      </span>
+
+      <span className={styles.slicedPages}>
+        {slicedPages.map((page, i) => {
+          const onPageChangedHandler = () => {
+            onPageChanged(page)
+          }
+          const isSelectedPage = page === currentPage
+          return (
+            <button
+              key={i}
+              className={`${styles.defaultBtnPage} ${isSelectedPage ? styles.selectedPage : ''}`}
+              type={'button'}
+              disabled={isSelectedPage}
+              onClick={onPageChangedHandler}
+            >
+              {page}
+            </button>
+          )
+        })}
+      </span>
+
+      <span className={styles.points} hidden={isEnd}>
+        ...
+      </span>
+
+      <button
+        className={`${styles.defaultBtnPage} ${styles.lastPage}`}
+        onClick={onLastChangedHandler}
+        hidden={isEnd}
+        type={'button'}
+      >
         {pageCount ? pageCount : ''}
       </button>
+
       <button
         onClick={onNextChanged}
-        className={styles.nextBtn}
+        className={`${styles.nextBtn} ${styles.arrow}`}
         disabled={currentPage === pageCount}
+        type={'button'}
       />
+
       {changePageSize && (
         <span className={styles.pageCount}>
           Show <Select options={selectOptions} value={pageSize} onChangeOption={changePageSize} />{' '}
